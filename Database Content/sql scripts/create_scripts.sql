43d1f52ca024b7f6889af20e78cb9567399529ed
Polina Radionova <halvaahaha@gmail.com>
2026-06-25 00:34:22 +0300

    Измененная схема БД после обсуждений 24.06

--- Database Content/sql scripts/create_scripts.sql
+++ Database Content/sql scripts/create_scripts.sql
@@ -1,44 +1,53 @@
-CREATE TABLE "User" (
+CREATE TABLE users (
     userID      SERIAL NOT NULL PRIMARY KEY
 );
 
-CREATE TABLE "Chat" (
-    chatID      SERIAL NOT NULL PRIMARY KEY,
-    userID      INT REFERENCES "User"(userID) ON DELETE CASCADE;
-    prompt      VARCHAR(500) NOT NULL,
-    csv         BYTEA NOT NULL,
-    title       VARCHAR(100) NOT NULL,
-    created_at  TIMESTAMPTZ DEFAULT now() NOT NULL
+CREATE TABLE csvs (
+    csvID       SERIAL NOT NULL PRIMARY KEY,
+    file        BYTEA NOT NULL
 );
 
-CREATE TABLE "Metrica" (
+CREATE TABLE jsons (
+    jsonID      SERIAL NOT NULL PRIMARY KEY,
+    file        JSONB NOT NULL
+);
+
+CREATE TABLE chats (
+    chatID      SERIAL NOT NULL PRIMARY KEY,
+    userID      INT REFERENCES users(userID) ON DELETE CASCADE,
+    csvID       INT REFERENCES csvs(csvid) ON DELETE CASCADE,
+    jsonID      INT REFERENCES jsons(jsonid) ON DELETE CASCADE,
+    title       VARCHAR(100) NOT NULL,
+    prompt      VARCHAR(500) NOT NULL  
+);
+
+CREATE TABLE metricas (
     metricaID       SERIAL NOT NULL PRIMARY KEY,
     name            VARCHAR(50)
-)
+);
 
-CREATE TABLE "Slide" (
+CREATE TABLE slides (
     slideID     SERIAL NOT NULL PRIMARY KEY,
-    chatID      INT REFERENCES "Chat"(chatID) ON DELETE CASCADE,
-    metricaID   INT REFERENCES "Metrica"(metricaID) ON DELETE CASCADE,
+    chatID      INT REFERENCES chats(chatID) ON DELETE CASCADE,
+    metricaID   INT REFERENCES metricas(metricaID) ON DELETE CASCADE,
     num         SMALLINT NOT NULL CHECK (num > 0),
     comment     VARCHAR(250) NULL
 );
 
-CREATE TABLE "Version" (
+CREATE TABLE versions (
     versionID       SERIAL NOT NULL PRIMARY KEY,
-    slideID         INT REFERENCES "Slide"(slideID) ON DELETE CASCADE,
-    version_num     SMALLINT NOT NULL CHECK (version_num >= 0),
-    json_content    JSONB NOT NULL,
-    edit_note       VARCHAR(250) NOT NULL,
+    slideID         INT REFERENCES slides(slideID) ON DELETE CASCADE,
+    json            JSONB NOT NULL,
+    prompt          VARCHAR(250) NOT NULL,
     created_at      TIMESTAMPTZ DEFAULT now() NOT NULL,
     is_final        BOOLEAN DEFAULT False NOT NULL
 );
 
-CREATE UNIQUE INDEX one_final_per_slide
-ON "Version"(slideID) WHERE is_final = TRUE;
-
-CREATE TABLE "Task" (
+CREATE TABLE tasks (
     taskID         SERIAL NOT NULL PRIMARY KEY,
-    versionID      INT REFERENCES "Version"(versionID) ON DELETE CASCADE,
-    status         INT DEFAULT 0 NOT NULL
+    chatID         INT REFERENCES chats(chatID) ON DELETE CASCADE,
+    versionID      INT REFERENCES versions(versionID) ON DELETE CASCADE,
+    type           INT NOT NULL,
+    prompt         VARCHAR(500) NOT NULL,
+    status         INT DEFAULT 0 NOT NULL           -- 0 - новая, 1 - в работе, 2 - завершена
 );
