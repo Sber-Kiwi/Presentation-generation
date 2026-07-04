66604265461c8e98fb2c9fdb2063d78c4477f31c
Shishmakov Vladimir <vova2005761@gmail.com>
2026-07-04 22:50:35 +0300

    add chat summary dto

---
+++ Backend/application/src/main/java/com/kiwi/dto/response/ChatSummaryDto.java
@@ -0,0 +1,23 @@
+package com.kiwi.dto.response;
+
+public class ChatSummaryDto {
+
+    String chatID;
+    String title;
+
+    public String getChatID() {
+        return chatID;
+    }
+
+    public void setChatID(String chatID) {
+        this.chatID = chatID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
