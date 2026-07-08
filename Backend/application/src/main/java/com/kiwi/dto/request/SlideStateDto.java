cf5e0693e24008aefda5f5e2e924807e3723f614
Shishmakov Vladimir <vova2005761@gmail.com>
2026-07-08 21:12:44 +0300

    add slideID field into schema

--- Backend/application/src/main/java/com/kiwi/dto/request/SlideStateDto.java
+++ Backend/application/src/main/java/com/kiwi/dto/request/SlideStateDto.java
@@ -5,10 +5,21 @@ import jakarta.validation.constraints.NotNull;
 public class SlideStateDto {
 
     @NotNull
+    private String slideID;
+
+    @NotNull
     private String selectedVersionID;
 
     @NotNull
     private Boolean inPresentation;
+
+    public String getSlideID() {
+        return slideID;
+    }
+
+    public void setSlideID(String slideID) {
+        this.slideID = slideID;
+    }
 
     public String getSelectedVersionID() {
         return selectedVersionID;
