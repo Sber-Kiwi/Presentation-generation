50aab235c99a6c82f8699564d97f05643d547b1e
Shishmakov Vladimir <vova2005761@gmail.com>
2026-07-08 21:12:15 +0300

    add support queries

--- Backend/application/src/main/java/com/kiwi/database/versions/VersionsRepository.java
+++ Backend/application/src/main/java/com/kiwi/database/versions/VersionsRepository.java
@@ -2,7 +2,10 @@ package com.kiwi.database.versions;
 
 import com.kiwi.database.chat.Chats;
 import com.kiwi.database.slides.Slides;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +13,12 @@ public interface VersionsRepository extends JpaRepository<Versions, Integer> {
     List<Versions> findBySlide(Slides slide);
 
     List<Versions> findBySlide_Chat_ChatIDAndIsFinalTrue (Integer chatId);
+
+    @Query("SELECT v from Versions v WHERE v.createdAt = " +
+            "(SELECT MAX(v2.createdAt) FROM Versions v2 WHERE v2.slide.slideID = v.slide.slideID) " +
+            "AND v.slide.chat.chatID = :chatId")
+    List<Versions> findLatestVersionsByChatId(@Param("chatId") Integer chatID);
+
+    @Query("SELECT v FROM Versions v WHERE v.slide.slideID = :slideId ORDER BY v.createdAt DESC")
+    List<Versions> findRecentVersionsBySlideId(@Param("slideId") Integer slideId, Pageable pageable);
 }
